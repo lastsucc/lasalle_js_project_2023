82916b658c967f4849c8187ebb32b64c749202aa
landogear17 <anthonylf797@gmail.com>
2023-11-27 10:54:20 -0500

    Home page created
    Have to change the images

--- final project/js/script.js
+++ final project/js/script.js
@@ -104,6 +104,21 @@ function updateTotals() {
 
 }
 
+var slidePosition = 0;
+SlideShow();
+
+
+function SlideShow() {
+  var i;
+  var slides = document.getElementsByClassName("Containers");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slidePosition++;
+  if (slidePosition > slides.length) {slidePosition = 1}
+  slides[slidePosition-1].style.display = "block";
+  setTimeout(SlideShow, 3500); // Change image every 2 seconds
+} 
 
 
 
